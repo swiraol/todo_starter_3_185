8520821a3584ac042fa0c1422919962c3b06d22d
Robert Tsui <rt.roberttsui@gmail.com>
2025-12-08 08:42:15 -0800

    added a 32 byte key

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import secrets
 from uuid import uuid4
 from functools import wraps
 from flask import (
@@ -24,7 +25,7 @@ from todos.utils import (
 )
 
 app = Flask(__name__)
-app.secret_key='secret1'
+app.secret_key = secrets.token_hex(32)
 
 def require_list(f):
     @wraps(f)
