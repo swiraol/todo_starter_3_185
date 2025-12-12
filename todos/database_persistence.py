bec53b328b17919d31a79b946004a17f5a986e96
Robert Tsui <rt.roberttsui@gmail.com>
2025-12-12 09:27:16 -0800

    refactored find_list to retrieve both the list and its todos

--- todos/database_persistence.py
+++ todos/database_persistence.py
@@ -29,9 +29,18 @@ class DatabasePersistence:
                 cursor.execute(query, (list_id,))
                 lst = dict(cursor.fetchone())
         
-        lst.setdefault('todos', [])
+        todos = self._find_todos_for_list(list_id)
+        lst.setdefault('todos', todos)
         return lst 
     
+    def _find_todos_for_list(self, list_id):
+        query = "SELECT * FROM todos WHERE list_id = %s"
+        logger.info("Executing query: %s with list_id: %s", query, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (list_id,))
+                return cursor.fetchall()
+
     def all_lists(self):
         query = "SELECT * FROM lists"
         logger.info("Executing query: %s", query)
@@ -41,7 +50,8 @@ class DatabasePersistence:
                 results = cursor.fetchall()
         lists = [dict(result) for result in results]
         for lst in lists:
-            lst.setdefault('todos', [])
+            todos = self._find_todos_for_list(lst['id'])
+            lst.setdefault('todos', todos)
         return lists 
     
     def create_new_list(self, title):
